358aa641631fab7bb404c9e9d3abfcfb26223b61
Meter Games <118026542+metergames@users.noreply.github.com>
2026-02-21 12:40:15 -0500

    Improve username validation; add addUser handler

--- src/models/validateUtils.ts
+++ src/models/validateUtils.ts
@@ -7,27 +7,36 @@ const MAXIMUM_USERNAME_LENGTH = 20;
 /**
  * Validates the format of user data for all fields.
  * @param username A unique identifier for the user. Must be between 3 and 20 characters.
+ *                 Allows letters, numbers and underscores, must have minimum of 1 letter.
  * @param firstName The user's name (only letters accepted).
  * @param email The user's email address.
  * @param isActive The active status of the user's account (true if active, false otherwise).
  * @returns True if username, first name, email and active status are valid.
  */
 function isValidUserData(username: string, firstName: string, email: string, isActive: boolean) {
-    if (!username || !validator.isAlphanumeric(username)) {
-        throw new InvalidInputError("Invalid username");
-    } else {
-        const usernameLength: number = username.length;
-        if (usernameLength < MINIMUM_USERNAME_LENGTH || usernameLength > MAXIMUM_USERNAME_LENGTH) {
-            throw new InvalidInputError(
-                `Username must be between ${MINIMUM_USERNAME_LENGTH} and ${MAXIMUM_USERNAME_LENGTH} characters`,
-            );
-        }
-    }
+    // Username
+    if (!username) throw new InvalidInputError("Invalid username");
 
+    if (!validator.isLength(username, { min: MINIMUM_USERNAME_LENGTH, max: MAXIMUM_USERNAME_LENGTH }))
+        throw new InvalidInputError(
+            `Username must be between ${MINIMUM_USERNAME_LENGTH} and ${MAXIMUM_USERNAME_LENGTH} characters`,
+        );
+
+    // a-z, A-Z, 0-9, _
+    if (!validator.matches(username, /^[a-zA-Z0-9_]+$/))
+        throw new InvalidInputError("Invalid username - must only contain letters, numbers or underscores");
+
+    const noUnderscoresUsername = username.replaceAll("_", "");
+    if (validator.isNumeric(noUnderscoresUsername) || noUnderscoresUsername === "")
+        throw new InvalidInputError("Invalid username - Username must contain at least one letter");
+
+    // First name
     if (!firstName || !validator.isAlpha(firstName)) throw new InvalidInputError("Invalid first name");
 
+    // Email
     if (!email || !validator.isEmail(email)) throw new InvalidInputError("Invalid email");
 
+    // Active status
     if (typeof isActive !== "boolean") throw new InvalidInputError("Invalid active status");
 
     return true;
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import { createServer, IncomingMessage, ServerResponse } from "http";
 import * as userModel from "./models/userModelMongoDb.js";
+import { InvalidInputError } from "./models/InvalidInputError.js";
+import { DuplicateError } from "./models/DuplicateError.js";
+import { DatabaseError } from "./models/DatabaseError.js";
 
 const PORT: number = 1339;
 const DB_NAME: string = "web-2-assignment-1-growth-mindset";
@@ -9,7 +12,28 @@ let initialized = userModel.initialize(DB_NAME, false, url);
 createServer(async function (request: IncomingMessage, response: ServerResponse): Promise<void> {
     await initialized;
     response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("Ended program");
+    response.write(await handleAddUser("alex_w", "Alex", "alex.w@example.com", true));
+    response.end("\n\nEnded program");
 }).listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
 });
+
+/**
+ * Adds a user via the model and returns a status message.
+ * @param username A unique identifier for the user. Must be between 3 and 20 characters.
+ * @param firstName The user's name (only letters accepted).
+ * @param email The user's email address.
+ * @param isActive The active status of the user's account (true if active, false otherwise).
+ * @returns A success or error message.
+ */
+async function handleAddUser(username: string, firstName: string, email: string, isActive: boolean): Promise<string> {
+    try {
+        const userToAdd: userModel.User = await userModel.addUser(username, firstName, email, isActive);
+        return `Successfully added user: ${userToAdd.username} - ${userToAdd.firstName} - ${userToAdd.email} - ${isActive ? "Active" : "Inactive"}`;
+    } catch (error) {
+        if (error instanceof InvalidInputError) return `Adding pokemon failed - Invalid input - ${error.message}`;
+        if (error instanceof DuplicateError) return `Adding pokemon failed - Duplicate error - ${error.message}`;
+        if (error instanceof DatabaseError) return `Adding pokemon failed - Database error - ${error.message}`;
+        return `Adding pokemon failed - Unknown error - ${error}`;
+    }
+}
